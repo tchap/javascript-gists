dd25c44e9e8e081c0ffff4ac7a1076ca2282bfa7
Ondřej Kupka <ondra.cap@gmail.com>
2016-03-04 11:33:37 +0100

    wip [ci skip]

--- rxjs/saga.js
+++ rxjs/saga.js
@@ -1,13 +1,15 @@
 const Rx = require('rx');
 
+const logOutput = observable => observable.forEach(
+  (x)   => console.log('Next: ' + JSON.stringify(x)),
+  (err) => console.log('Error: ' + err),
+  ()    => console.log('Completed')
+);
+
 var actions$ = Rx.Observable.timer(0, 1000)
   .timeInterval()
   .map(x => ({
     action: `Stories[${x.value % 3}].Edit`,
   }));
 
-actions$.subscribe(
-  (x) => console.log('Next: ' + JSON.stringify(x)),
-  (err) => console.log('Error: ' + err),
-  () => console.log('Completed')
-);
+logOutput(actions$);

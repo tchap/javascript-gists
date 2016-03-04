bebd7c12a351975e4c98ad11928f510d66f117fd
Ondřej Kupka <ondra.cap@gmail.com>
2016-03-04 11:27:23 +0100

    wip [ci skip]

--- rxjs/saga.js
+++ rxjs/saga.js
@@ -2,6 +2,9 @@ const Rx = require('rx');
 
 var actions$ = Rx.Observable.timer(0, 1000)
   .timeInterval()
+  .map(x => ({
+    action: `Stories[${x.value % 3}].Edit`,
+  }));
 
 actions$.subscribe(
   (x) => console.log('Next: ' + JSON.stringify(x)),
